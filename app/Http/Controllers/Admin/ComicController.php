6a9524e4ef2c5b94050fbf88da81512805de2244
dav100ide <oreglio.dev@gmail.com>
2023-01-19 18:00:00 +0100

    problema movie o comic?

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -16,8 +16,7 @@ class ComicController extends Controller
     public function index()
     {
         $comics = Comic::all();
-
-        return view('movies.index', compact('comics'));
+        return view('comics.index', compact('comics'));
     }
 
     /**
